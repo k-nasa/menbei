0606612b642a64ec915d3da3fe43298b94ba03dc
nasa <htilcs1115@gmail.com>
2019-01-19 11:09:58 +0900

    Add IssueLink::default test

--- src/issue_link.rs
+++ src/issue_link.rs
@@ -7,3 +7,26 @@ pub struct IssueLink {
     labels: Vec<String>,
     projects: Vec<String>,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn issue_link_default() {
+        let issue_link = IssueLink::default();
+
+        assert_eq!(
+            issue_link,
+            IssueLink {
+                organization: None,
+                repository: None,
+                title: None,
+                body: None,
+                assignees: Vec::new(),
+                labels: Vec::new(),
+                projects: Vec::new(),
+            }
+        );
+    }
+}
